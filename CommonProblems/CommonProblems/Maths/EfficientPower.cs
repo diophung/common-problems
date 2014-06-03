7db4f56ed6c41bee5f463a7d80bbe17ba29afbc8
Dio Phung <dio@gtthtt.com>
2014-06-03 16:54:22 +0800

    trying more efficient way than Math.Pow()
    Ans: Math.exp(a*Math.Log(b))

---
+++ CommonProblems/CommonProblems/Maths/EfficientPower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonProblems.Maths
+{
+	public class EfficientPower
+	{
+		public Int64 NormalPowerMethod(double x, int toPower)
+		{
+			return (Int64)Math.Pow(x, toPower);
+		}
+
+		public Int64 EfficientPowerMethod(double x, int toPower)
+		{
+			return (Int64)Math.Exp(Math.Log(x) * toPower);
+		}
+	}
+}
---
+++ CommonProblems/CommonProblems/Program.cs
@@ -0,0 +1,36 @@
+﻿using CommonProblems.Maths;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonProblems
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			EfficientPower ep = new EfficientPower();
+			Console.WriteLine("Enter x:");
+			double x = Double.Parse(Console.ReadLine());
+			Console.WriteLine("Enter power: ");
+			int toPower = Int32.Parse(Console.ReadLine());
+			Stopwatch timer = new Stopwatch();
+			timer.Start();
+			Console.WriteLine(string.Format("{0} to the power of {1}= {2}", x,toPower, ep.NormalPowerMethod(x,toPower)));
+			timer.Stop();
+			Console.WriteLine("Normal power time =" + timer.ElapsedTicks);
+			
+			//-------------- efficient
+			timer.Reset();
+			timer.Start();
+			Console.WriteLine(string.Format("Efficient power: {0} to the power of {1}= {2}", x, toPower, ep.EfficientPowerMethod(x, toPower)));
+			timer.Stop();
+			Console.WriteLine("Efficient power time =" + timer.ElapsedTicks);
+
+			Console.ReadLine();
+		}
+	}
+}
