ff4066bbfb643c42581d888df713317d875560af
Dio Phung <dio@gtthtt.com>
2014-07-29 00:02:02 +0800

    Add new string algo and tests

--- CommonProblems/CommonProblems/Algorithm/StringUnique.cs
+++ CommonProblems/CommonProblems/Algorithm/StringUnique.cs
@@ -1,4 +1,33 @@
 public class StringUnique {
 	public const string Alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
-	
+
+	bool isUnique(string str) {
+		if (string.IsNullOrEmpty(str)) {
+			return false;
+		}
+		HashTable<char, int> charsMap = new HashTable<char,int>();
+		
+		char[] chars = Alphabet.ToCharArray();
+		
+		//store the count of each character
+		for(int i =0; i< chars.Length ; i++){
+			for(int j= 0; j< str.Length; j++){
+				if (charsMap[chars[j]] == null){
+					charsMap[chars[j]] = 1;
+				}
+				else {
+					charsMapp[chars[j]] += 1;
+				}
+			}			
+		}
+		
+		//check if any char is missing
+		for(int i =0; i< chars.Length ; i++){
+			if (charsMap[chars[i]] < 1) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
